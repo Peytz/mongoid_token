16daba7547a84292c04556cb26ac31d65d0df5b2
Nikolaj Nikolajsen <nikolaj@nikolajsen.net>
2025-02-03 12:02:32 +0100

    Silence warnings from Zeitwerk caused by collisions with the mongoid gem.

--- lib/mongoid_token.rb
+++ lib/mongoid_token.rb
@@ -3,6 +3,14 @@
 require "zeitwerk"
 require "active_support/concern"
 loader = Zeitwerk::Loader.for_gem
+
+# Zeitwerk attempts to autoload constants that are already explicitly defined
+# because the mongoid_token gem has a directory named mongoid, which collides
+# with the mongoid gem. Ignore the mongoid directory and the version file to
+# prevent this.
+loader.ignore("#{__dir__}/mongoid")
+loader.ignore("#{__dir__}/version.rb")
+
 loader.setup
 
 module MongoidToken
